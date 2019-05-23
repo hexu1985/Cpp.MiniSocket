754ba85d379f88cb7c5322201a555143144ade12
hexu <hexu_1985@sina.com>
2019-05-23 11:18:03 +0800

    add doc for SocketAddress class

--- include/mini_socket.hpp
+++ include/mini_socket.hpp
@@ -102,18 +102,65 @@ private:
  */
 class SocketAddress {
 public:
+    /**
+     * @brief 构造一个空的SocketAddress
+     */
     SocketAddress(); 
+
+    /**
+     * @brief 根据指定ip+port构造一个SocketAddress
+     *
+     * @param address ip地址的字符串
+     * @param port 端口号
+     *
+     * @note 如果地址无效, 会抛出SocketException异常
+     */
     SocketAddress(const char *address, uint16_t port); 
+
+    /**
+     * @brief 从sockaddr指针转换成一个SocketAddress
+     *
+     * @param addrVal 指向sockaddr的指针
+     * @param addrLenVal 实际sockaddr的长度
+     */
     SocketAddress(sockaddr *addrVal, socklen_t addrLenVal);
 
+    /**
+     * @brief 根据指定ip+port重新设置一个SocketAddress
+     *
+     * @param address ip地址的字符串
+     * @param port 端口号
+     *
+     * @return 如果是有效地址, 返回true; 否则返回false
+     */
     bool setAddressPort(const char *address, uint16_t port); 
 
+    /**
+     * @brief 将SocketAddress转换成可打印格式
+     *
+     * @return ipv4: xxx.xxx.xxx.xxx:port, ipv6: [xxx:xxx:...:xxx]:port
+     */
     std::string toString() const;
 
+    /**
+     * @brief 获取可打印地址和port号
+     *
+     * @return 包含地址和端口号的元组
+     */
     std::tuple<std::string, uint16_t> getAddressPort() const;
 
+    /**
+     * @brief 转换成sockaddr指针
+     *
+     * @return 指向sockaddr的指针
+     */
     sockaddr *getSockaddr() const; 
 
+    /**
+     * @brief 获取sockaddr实际的长度
+     *
+     * @return sockaddr长度
+     */
     socklen_t getSockaddrLen() const; 
 
 private:
@@ -121,16 +168,16 @@ private:
     socklen_t addrLen_ = sizeof (sockaddr_storage);
 };
 
-// IPVersion
+/// IP版本号
 enum class IPVersion {
-    IPv4 = AF_INET,
-    IPv6 = AF_INET6,
+    IPv4 = AF_INET,     /*!< IPv4版本 */
+    IPv6 = AF_INET6,    ///< IPv6版本
 };
 
-// TransportLayerType 
+/// 传输层协议类型
 enum class TransportLayerType {
-    TCP = SOCK_STREAM,
-    UDP = SOCK_DGRAM,
+    TCP = SOCK_STREAM,  /*!< TCP协议 */
+    UDP = SOCK_DGRAM,   ///< UDP协议
 };
 
 // Socket
