766437b61cb476d7d4ebd82a265917bdb3ab215a
hexu <hexu_1985@sina.com>
2019-05-24 14:16:34 +0800

    adjust indent

--- src/mini_socket.cpp
+++ src/mini_socket.cpp
@@ -369,41 +369,41 @@ public:
     }
 
 protected:
-  int_type overflow(int_type c = Traits::eof()) 
-  {
-      this->sync();
+    int_type overflow(int_type c = Traits::eof()) 
+    {
+        this->sync();
 
-      if (c != Traits::eof()) {
-          this->sputc(Traits::to_char_type(c));
-      }
+        if (c != Traits::eof()) {
+            this->sputc(Traits::to_char_type(c));
+        }
 
-      return 0;
-  }
+        return 0;
+    }
 
-  int sync() 
-  {
-      sock_->sendAll(outBuffer_, (this->pptr() - outBuffer_) * sizeof(CharT));
-      this->setp(outBuffer_, outBuffer_ + sizeof(outBuffer_));
-      return 0;
-  }
+    int sync() 
+    {
+        sock_->sendAll(outBuffer_, (this->pptr() - outBuffer_) * sizeof(CharT));
+        this->setp(outBuffer_, outBuffer_ + sizeof(outBuffer_));
+        return 0;
+    }
 
-  int_type underflow() 
-  {
-      size_t len = sock_->recv(inBuffer_, sizeof(inBuffer_) * sizeof(CharT));
+    int_type underflow() 
+    {
+        size_t len = sock_->recv(inBuffer_, sizeof(inBuffer_) * sizeof(CharT));
 
-      if (len == 0) {
-          return Traits::eof();
-      }
+        if (len == 0) {
+            return Traits::eof();
+        }
 
-      this->setg(inBuffer_, inBuffer_, inBuffer_ + len / sizeof(CharT));
-      return this->sgetc();
-  }
+        this->setg(inBuffer_, inBuffer_, inBuffer_ + len / sizeof(CharT));
+        return this->sgetc();
+    }
 
 private:
-  TCPSocket *sock_;
+    TCPSocket *sock_;
 
-  CharT inBuffer_[1024];
-  CharT outBuffer_[1024];
+    CharT inBuffer_[1024];
+    CharT outBuffer_[1024];
 };
 
 // TCPSocket 
