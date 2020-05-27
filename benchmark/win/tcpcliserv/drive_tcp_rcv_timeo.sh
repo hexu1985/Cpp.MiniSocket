03f835b0c9444eb54ded72aaeff1d639f3a8b20e
hexu <hexu@sensetime.com>
2020-05-27 15:24:08 +0800

    add drive_tcp_rcv_timeo.sh

---
+++ benchmark/win/tcpcliserv/drive_tcp_rcv_timeo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SRV_PORT=$(($RANDOM + 1024))
+
+./tcpserv_rcv_timeo $SRV_PORT &
+SRV_PID=$!
+
+sleep 1
+
+./tcpcli_rcv_timeo 127.0.0.1 $SRV_PORT <<EOF
+hello
+world
+bye
+EOF
+
+kill $SRV_PID
+
