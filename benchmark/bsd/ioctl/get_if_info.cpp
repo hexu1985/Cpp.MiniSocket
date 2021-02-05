a4b799266daaa86f7c27c33b2a3e9b1d0f31b210
hexu <hexu_1985@sina.com>
2021-02-05 16:34:38 +0800

    add get_if_info

--- benchmark/bsd/ioctl/get_if_info.cpp
+++ benchmark/bsd/ioctl/get_if_info.cpp
@@ -3,13 +3,21 @@
 #include <arpa/inet.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
+#include <net/if.h>
+#include <netinet/if_ether.h>
 #include <unistd.h>
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
 #include "err_quit.hpp"
+#include "sock_ntop.hpp"
+
+void get_if_info(int sockfd);
+void get_if_hwaddr(int sockfd, const char *ifname);
+void get_if_ipaddr(int sockfd, const char *ifname);
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +26,13 @@ int main(int argc, char *argv[])
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
         err_quit("socket error");
 
+    get_if_info(sockfd);
+
+    return 0;
+}
+
+void get_if_info(int sockfd)
+{
     struct ifconf ifc;
     struct ifreq ifr_arr[16];
 
@@ -31,10 +46,75 @@ int main(int argc, char *argv[])
 
     int if_count = ifc.ifc_len / (sizeof(struct ifreq));
     printf("interface num = %d\n", if_count);
+    printf("\n");
 
     for (int i = 0; i < if_count; i++) {
+        printf("%s", ifr_arr[i].ifr_name);
+
+        printf("\n\t\t");
+        get_if_hwaddr(sockfd, ifr_arr[i].ifr_name);
+
+        printf("\n\t\t");
+        get_if_ipaddr(sockfd, ifr_arr[i].ifr_name);
+
+        printf("\n\n");
+
     }
 
+}
 
-    return 0;
+void get_if_hwaddr(int sockfd, const char *ifname)
+{
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+
+    if (ioctl(sockfd, SIOCGIFHWADDR, &ifr) < 0) 
+        err_quit("ioctl error: SIOCGIFHWADDR");
+
+    const uint8_t *mac = NULL;
+    printf("Link encap:");
+    switch(ifr.ifr_hwaddr.sa_family) {
+        case ARPHRD_LOOPBACK:
+            printf("LOOPBACK");
+            break;
+        case ARPHRD_NETROM:
+            printf("NETROM");
+            break;
+        case ARPHRD_ETHER:
+            mac = (const uint8_t *) ifr.ifr_hwaddr.sa_data;   // ETH_ALEN
+            printf("ETHER");
+            printf("\t");
+            printf("MAC address: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x",
+                        *mac, *(mac+1), *(mac+2), *(mac+3), *(mac+4), *(mac+5));
+            break;
+        case ARPHRD_PPP:
+            printf("PPP");
+            break;
+        case ARPHRD_EETHER:
+            printf("EETHER");
+            break;
+        case ARPHRD_IEEE802:
+            printf("IEEE802");
+            break;
+        default:
+            printf("UNKNOWN(%d)", ifr.ifr_hwaddr.sa_family);
+            break;
+    }
+}
+
+void get_if_ipaddr(int sockfd, const char *ifname)
+{
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+
+    if (ioctl(sockfd, SIOCGIFADDR, &ifr) < 0) {
+        if (errno == EADDRNOTAVAIL) {   // No IPv4 address assigned
+            return;
+        }
+        err_quit("ioctl error: SIOCGIFADDR");
+    }
+
+    printf("inet addr:%s", sock_ntop(&ifr.ifr_addr, 0));
 }
